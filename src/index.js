65aad5718fb0b973b9915a12acadecf3f4779494
Patrick König <p.koenig@fulltime-ecommerce.de>
2023-12-06 14:52:47 +0100

    init

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { NotificationsContext as NotificationsContextNk, NotificationsProvider as NotificationsProviderNk } from 'Notifications';
+import "./index.css";
+
+export const NotificationsContext = NotificationsContextNk;
+export const NotificationsProvider = NotificationsProviderNk;
